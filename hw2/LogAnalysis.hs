7d4763fe2a3ec79de4c6fa6c70cabe49d0b086a3
Daniel Tuveson <danieltuveson@gmail.com>
2019-08-21 21:17:45 -0700

    finished parser

--- hw2/LogAnalysis.hs
+++ hw2/LogAnalysis.hs
@@ -1,18 +1,25 @@
-module LogAnalysis where
+module Hw2.LogAnalysis where
 
-import Log
+import Hw2.Log 
+import Text.Read (readMaybe)
+
+parse :: String -> [LogMessage]
+parse = map parseMessage . lines 
 
 parseMessage :: String -> LogMessage
-parseMessage str = Unknown str
+parseMessage = parseWords . words
 
+parseWords :: [String] -> LogMessage
+parseWords line@("E" : errNum : tStamp : strs) =
+            case (readMaybe errNum :: Maybe Int, readMaybe tStamp :: Maybe Int) of 
+                (Just en, Just ts) -> LogMessage (Error en) ts (unwords strs)
+                _                  -> Unknown (unwords line)
+parseWords line@(mType : tStamp : strs) =
+            case readMaybe tStamp :: Maybe Int of 
+                Just ts -> LogMessage mt ts (unwords strs)
+                    where mt 
+                             | mType == "I" = Info 
+                             | otherwise    = Warning
+                Nothing -> Unknown (unwords line)
+parseWords line = Unknown (unwords line)
 
-messageType :: String -> Maybe (MessageType, String)
-messageType ('I' : ' ' : str) = Just (Info, str)
-messageType ('W' : ' ' : str) = Just (Warning, str)
-messageType ('E' : ' ' : str) = Nothing
-
-messageType s = Nothing
-
-
--- isInt :: String -> Int
--- isInt str = foldr isDigit False str
